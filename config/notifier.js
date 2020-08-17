18ed5c11a16c6925e2dfa3a00ddbac18eb3f6b75
jasmaa <jasonmaa3955@gmail.com>
2020-08-17 19:59:45 -0400

    Link up notification on match

---
+++ config/notifier.js
@@ -0,0 +1,62 @@
+const twilioClient = require('./twilioClient');
+const sgMail = require('@sendgrid/mail');
+
+require('dotenv').config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+/**
+ * Email and SMS notifier
+ */
+class Notifier {
+    constructor() {
+        this.sgSender = process.env.SENDGRID_SENDER;
+        this.twilioNumber = process.env.TWILIO_NUMBER;
+    }
+
+    /**
+     * Notifies both users on match
+     * 
+     * @param {*} user 
+     * @param {*} crushUser 
+     */
+    async notifyMatchBoth(user, crushUser) {
+        await Promise.all([
+            this.notifyMatch(user, crushUser),
+            this.notifyMatch(crushUser, user),
+        ]);
+    }
+
+    /**
+     * Notifies user of crush user
+     * 
+     * @param {*} user 
+     * @param {*} crushUser 
+     */
+    async notifyMatch(user, crushUser) {
+
+        const msg = `You've have matched with ${crushUser.name}!`;
+
+        // Notify via email
+        if (user.isEmailVerified) {
+            await sgMail.send({
+                to: user.email,
+                from: this.sgSender,
+                subject: 'You got a match!',
+                text: msg,
+            });
+        }
+
+        // Notify via SMS
+        if (user.isPhoneVerified) {
+            await twilioClient.messages
+                .create({
+                    to: user.phone,
+                    from: this.twilioNumber,
+                    body: msg,
+                });
+        }
+    }
+}
+
+module.exports = Notifier;
--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const { User } = require('../config/db');
 const { authorizeUser, authorizeAccount, getUserInfo } = require('../middleware');
 const twilioClient = require('../config/twilioClient');
+const Notifier = require('../config/notifier');
 const { formatPhone } = require('../utils');
+
+const notifier = new Notifier();
 
 router.route('/createAccount')
     .get(authorizeUser, (req, res) => {
@@ -128,17 +131,21 @@ router.post('/addCrush', authorizeUser, authorizeAccount, async (req, res) => {
 
     const { crushUID } = req.body;
     if (crushUID && crushUID.length > 0 && crushUID !== req.user.uid) {
-        await User.updateOne({ uid: req.user.uid }, { $addToSet: { crushes: crushUID } });
 
-        // Matches users if both mutual crush
+        const user = await User.findOneAndUpdate(
+            { uid: req.user.uid },
+            { $addToSet: { crushes: crushUID } },
+            { new: true },
+        );
+
+        // Matches and notifies both users on mutual crush
         const crushUser = await User.findOne({ uid: crushUID });
-        if (crushUser && crushUser.crushes.includes(req.user.uid)) {
-            await Promise.all([
-                User.updateOne({ uid: req.user.uid }, { $addToSet: { matches: crushUID } }),
-                User.updateOne({ uid: crushUID }, { $addToSet: { matches: req.user.uid } }),
-            ]);
+        if (crushUser && crushUser.crushes.includes(req.user.uid) && !crushUser.matches.includes(req.user.uid)) {
 
-            // TODO: notify both users
+            await User.updateOne({ uid: req.user.uid }, { $addToSet: { matches: crushUID } });
+            await User.updateOne({ uid: crushUID }, { $addToSet: { matches: req.user.uid } });
+
+            await notifier.notifyMatch(user, crushUser);
         }
     }
 
@@ -153,10 +160,8 @@ router.post('/deleteCrush', authorizeUser, authorizeAccount, async (req, res) =>
     // Removes crush if users were matched
     const crushUser = await User.findOne({ uid: crushUID });
     if (crushUser && crushUser.matches.includes(req.user.uid)) {
-        await Promise.all([
-            User.updateOne({ uid: req.user.uid }, { $pull: { matches: crushUID } }),
-            User.updateOne({ uid: crushUID }, { $pull: { matches: req.user.uid } }),
-        ]);
+        await User.updateOne({ uid: req.user.uid }, { $pull: { matches: crushUID } });
+        await User.updateOne({ uid: crushUID }, { $pull: { matches: req.user.uid } });
     }
 
     res.redirect('/');
