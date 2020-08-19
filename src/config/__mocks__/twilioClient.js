4b5cb13e2fa4af0ed36aaead7e77441544d84f38
jasmaa <jasonmaa3955@gmail.com>
2020-08-19 12:19:21 -0400

    Update twilio client mock, add verification controller tests

--- src/config/__mocks__/twilioClient.js
+++ src/config/__mocks__/twilioClient.js
@@ -1,9 +1,19 @@
-const create = jest.fn();
+const verifications = {
+    create: jest.fn(),
+};
 
-const verifications = { create };
+const verificationChecks = {
+    create: jest.fn(({ to, code }) => {
+        if (code === 'goodCode') {
+            return { valid: true };
+        } else {
+            throw new Error('Invalid code');
+        }
+    }),
+}
 
 const services = () => {
-    return { verifications }
+    return { verifications, verificationChecks }
 };
 
 const verify = { services };
